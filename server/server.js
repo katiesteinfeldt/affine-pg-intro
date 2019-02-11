de5eee83680fd03ea858051bea0fb68a335df886
Katie Mangan <katie.mangan1@gmail.com>
2019-02-11 15:09:01 -0600

    added pg

---
+++ server/server.js
@@ -0,0 +1,24 @@
+console.log('hello pg');
+
+const pg = require('pg');
+
+const pool = pg.Pool({
+    host: 'localhost', //where is your database?
+    port: 5432, //what port is your database on? (Almost always 5432)
+    database: 'music_library', // the name of your database
+    max: 10, // how many connections (queries) at one time
+    idleTimeoutMillis: 30000 // 30 seconds to try to connect, otherwise cancel query
+});
+
+pool.on('connect', () => {
+    console.log('Postgresql connected');
+});//only happens when we run a query
+
+pool.on('error', (error) => {
+    console.log('Error with postgres pool', error);
+});//only happens when we run a query
+
+pool.query('SELECT * FROM "songs"')
+    .then((result) => {
+        console.log(result);
+    });
